d08189368bbd273b4cca4fefb56af067e01acac4
Ekoue Gaba <ekoue.gaba@yahoo.com>
2024-06-20 18:13:49 -0400

    finished

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+
+
+let createAcctBttn = document.querySelector('.create-account-bttn')
+
+
+createAcctBttn.addEventListener('click', ()=>{
+    let passwordField1 = document.querySelector('.first-password');
+    let passwordField2 = document.querySelector('.second-password');
+    let field1 = passwordField1.value;
+    let field2 = passwordField2.value;
+
+    if (field1 != field2){
+        
+    }
+
+    console.log(password);
+});
